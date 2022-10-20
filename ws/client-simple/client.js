4c8ae868a554025e6efc79ff68782dda9a837383
Matheus Valle <mdovallesilva@gmail.com>
2022-10-20 16:33:19 -0300

    Alterando Address para conectar com glitch

--- ws/client-simple/client.js
+++ ws/client-simple/client.js
@@ -1,9 +1,14 @@
 
 const WebSocket = require('ws');
 
-const serverAddress = "ws://127.0.0.1:5000"
+//const serverAddress = "ws://127.0.0.1:5000"
+const serverAddress = 'wss://simple-websocket-echo2.glitch.me/';
 
-const ws = new WebSocket(serverAddress);
+const ws = new WebSocket(serverAddress, {
+    headers:{
+        "user-agent": "Mozilla"
+    }
+});
 
 ws.on('open', function(){
     ws.send("Hello server!");
